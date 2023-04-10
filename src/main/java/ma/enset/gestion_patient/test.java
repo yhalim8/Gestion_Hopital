f92f6c2c8b658bdc652f5ee3e5c648b89caa874a
yhalim8 <yhalim8@gmail.com>
2023-04-11 01:22:52 +0200

    first commit

---
+++ src/main/java/ma/enset/gestion_patient/test.java
@@ -0,0 +1,12 @@
+package ma.enset.gestion_patient;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class test {
+    public static void main(String[] args) throws ParseException {
+
+    }
+}
